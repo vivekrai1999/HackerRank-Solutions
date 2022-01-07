d244b58392636ca25bbe4485bc2656d0f5e3fce6
Vivek Rai <96563624+vivekrai1999@users.noreply.github.com>
2022-01-07 16:18:13 +0530

    Added Better Solution

--- average-marks.py
+++ average-marks.py
@@ -1,9 +1,10 @@
 # solution notes:
 # name, *line means that the first element of the list obtained by input().split() will be assigned to name and rest of the items will be assigned to the line(list of rest items)
 # interger marks further converted into list of float using map and list method
-# approch: iterate through all the keys in dictionary
-#          check if the key name matches the query name
-#          if matches then access the value by using key
+# approch: directly access the value of the query name from dictionary
+#          add all items of list using sum()
+#          divide the sum with the number of items in the score list that is the length of the score list.
+#          print the average with 2 decimal float precision.
 # float precision use: "{:.2f}".format(data_to_be_formatted)
 
 if __name__ == '__main__':
@@ -15,6 +16,4 @@ if __name__ == '__main__':
         student_marks[name] = scores # creating dictionary
     query_name = input()
     
-    for key in student_marks:
-        if key == query_name:
-            print("{:.2f}".format(sum(student_marks[key])/3))
+    print("{:.2f}".format(sum(student_marks[query_name]/len(scores)))
