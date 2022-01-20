c09c8fcda0c9f2dcd845e7224a277ebd3a0fb011
Vivek Rai <96563624+vivekrai1999@users.noreply.github.com>
2022-01-20 16:35:59 +0530

    Create symmetric-difference.py

---
+++ symmetric-difference.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+_ = int(input())
+set_m = set(map(int, input().split()))
+_ = int(input())
+set_n = set(map(int, input().split()))
+s_difference1 = []
+s_difference1.extend(set_m.difference(set_n))
+s_difference1.extend(set_n.difference(set_m))
+for item in sorted(s_difference1):
+    print(item)
