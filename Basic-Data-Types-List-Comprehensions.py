f361d749ba107def97a6068a725279bade115c9f
Vivek Rai <96563624+vivekrai1999@users.noreply.github.com>
2022-02-11 07:50:24 +0530

    Create Basic-Data-Types-List-Comprehensions.py

---
+++ Basic-Data-Types-List-Comprehensions.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    data = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1)]
+    print([item for item in data if sum(item)!=n])
