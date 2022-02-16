a1db907a748e1b5bac13ee33ecd4abb3e7c9acad
Vivek Rai <96563624+vivekrai1999@users.noreply.github.com>
2022-02-16 22:28:14 +0530

    Create Math-Triangle-Quest.py

---
+++ Math-Triangle-Quest.py
@@ -0,0 +1,8 @@
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print((10**i//9)**2)
+   
+# 1*1 = 1
+# 11*11 = 121
+# 111*111 = 12321
+# 1111*1111 = 1234321
+# 11111*11111 = 123454321
